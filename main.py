dcc09fac278bc8db0e71a9aeb35ae40e897fb131
Max-Greenberg25 <maxmgreenberg25@gmail.com>
2025-03-25 17:37:13 -0400

    Added Player Movement logic

--- main.py
+++ main.py
@@ -2,28 +2,35 @@ import pygame
 from constants import *
 from player import Player
 
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+
     player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
-    
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-    
-
+  
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        screen.fill("black")
-        player1.draw(screen)
-        pygame.display.flip()
 
+        updateable.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+        
         
         dt = clock.tick(60) / 1000
 

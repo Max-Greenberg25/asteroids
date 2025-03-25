c8267ed1eeb465fdef32d68b4cf5aad1d200db1b
Max-Greenberg25 <maxmgreenberg25@gmail.com>
2025-03-25 16:18:00 -0400

    Added player and circleshape object

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
-import pygame as gm
+import pygame
 from constants import *
+from player import Player
 
 def main():
-    gm.init()
-    screen = gm.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    
+
     while True:
-        for event in gm.event.get():
-            if event.type == gm.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+            
+        screen.fill("black")
+        player1.draw(screen)
+        pygame.display.flip()
 
-
-        gm.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":

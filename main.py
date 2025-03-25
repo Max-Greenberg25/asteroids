80353ecdb9617617a05093be94fa2457ebb949a7
Max-Greenberg25 <maxmgreenberg25@gmail.com>
2025-03-25 15:17:06 -0400

    Added main loop and screen settings

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-import pygame
+import pygame as gm
+from constants import *
 
 def main():
+    gm.init()
+    screen = gm.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
+    while True:
+        for event in gm.event.get():
+            if event.type == gm.QUIT:
+                return
+        screen.fill((0,0,0))
+
+
+        gm.display.flip()
 
 
 if __name__ == "__main__":
